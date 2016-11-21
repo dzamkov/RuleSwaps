d6adb59ba13ac6e5504a0a176e134269e1c69b5b
Dmitry Zamkov <Dmitry Zamkov>
2016-11-20 22:33:36 -0600

    Improved loading parallelism

--- source/client.js
+++ source/client.js
@@ -9,30 +9,42 @@ function ajax(request, callback) {
 	xhttp.send(JSON.stringify(request));
 }
 
-// Parse the Id of the game we're looking at from the URL
-window.onload = function() {
-	ajax({ messageType: "intro" }, function(response) {
-		let setup = response.setup;
-		let inteface = new Interface(setup, 0, {
-			deckDraw: document.getElementById("deck-draw"),
-			deckDiscard: document.getElementById("deck-discard"),
-			deckPlay: document.getElementById("deck-play"),
-			
-			constitutionNumbers: document.getElementById("section-constitution-numbers"),
-			constitutionList: document.getElementById("section-constitution-list"),
-			log: document.getElementById("section-log"),
-			
-			inputBoolean: document.getElementById("input-boolean"),
-			inputBooleanYes: document.getElementById("input-boolean-yes"),
-			inputBooleanNo: document.getElementById("input-boolean-no"),
-			
-			inputExpression: document.getElementById("input-expression"),
-			inputExpressionList: document.getElementById("input-expression-list"),
-			inputExpressionAccept: document.getElementById("input-expression-accept"),
-			inputExpressionPass: document.getElementById("input-expression-pass"),
-			
-			selfHand: document.getElementById("section-player-self-hand")
-		});
-		inteface.run(null);
+// Starts the game given the response to the "intro" request.
+function start(response) {
+	let setup = response.setup;
+	let inteface = new Interface(setup, 0, {
+		deckDraw: document.getElementById("deck-draw"),
+		deckDiscard: document.getElementById("deck-discard"),
+		deckPlay: document.getElementById("deck-play"),
+		
+		constitutionNumbers: document.getElementById("section-constitution-numbers"),
+		constitutionList: document.getElementById("section-constitution-list"),
+		log: document.getElementById("section-log"),
+		
+		inputBoolean: document.getElementById("input-boolean"),
+		inputBooleanYes: document.getElementById("input-boolean-yes"),
+		inputBooleanNo: document.getElementById("input-boolean-no"),
+		
+		inputExpression: document.getElementById("input-expression"),
+		inputExpressionList: document.getElementById("input-expression-list"),
+		inputExpressionAccept: document.getElementById("input-expression-accept"),
+		inputExpressionPass: document.getElementById("input-expression-pass"),
+		
+		selfHand: document.getElementById("section-player-self-hand")
 	});
+	inteface.run(null);
 }
+
+// Handle loading
+let loaded = false;
+let introResponse = null;
+
+window.onload = function() {
+	loaded = true;
+	if (loaded && introResponse) start(introResponse);
+}
+
+ajax({ messageType: "intro" }, function(response) {
+	introResponse = response;
+	if (loaded && introResponse) start(introResponse);
+});
